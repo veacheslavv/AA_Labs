44380b2a32e2817ab92e3a3c448da8d55c432cd3
veacheslavv <144955981+veacheslavv@users.noreply.github.com>
2026-02-24 15:26:42 +0200

    Implement insertion sort algorithm

---
+++ Lab2/insertionSort.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):
+  a = arr[:] 
+  n = len(a)
+  for i in range(1, n):
+    key = a[i]
+    j = i- 1
+    while j >= 0 and a[j] > key:
+      a[j + 1] = a[j]
+      j-= 1
+      a[j + 1] = key
+return a
